07b9c1ebbbd132bf6630ded8cf0274b0111b890b
Luis Reyes <luisreyesxv@yahoo.com>
2020-06-14 23:03:33 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -3,11 +3,11 @@
 require 'pry'
 def key_for_min_value(name_hash)
   binding.pry
-      lowest_value = name_hash.first[1]
-      answer = name_hash.first[0]
+      lowest_value = nil
+      answer = nil]
       name_hash.each do |key,value|
 
-                    if value < lowest_value
+                    if value < lowest_value || lowest_value == nil
                        lowest_value = value
                        answer = key
 
