0cd67ca8a74f7c97a26aee7ed4a7c0cfe69e7872
Luis Reyes <luisreyesxv@yahoo.com>
2020-06-14 23:05:37 +0000

    Automatically backed up by Learn

--- key_for_min.rb
+++ key_for_min.rb
@@ -7,7 +7,7 @@ def key_for_min_value(name_hash)
       answer = nil
       name_hash.each do |key,value|
 
-                    if lowest_value == nil || value < lowest_value 
+                    if lowest_value == nil || value < lowest_value
                        lowest_value = value
                        answer = key
 
